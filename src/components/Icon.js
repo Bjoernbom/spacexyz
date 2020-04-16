4d7e9a95c0e88207a0e333896ec07c939c6c945f
Ludvig <l.lundh@hotmail.com>
2020-04-16 22:10:01 +0200

    add fontello

---
+++ src/components/Icon.js
@@ -0,0 +1,7 @@
+import {createIconSetFromFontello} from 'react-native-vector-icons';
+
+import config from '../fontello/config.json';
+
+const Icon = createIconSetFromFontello(config);
+
+export default Icon;
