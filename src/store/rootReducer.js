e67870b5d91586bca755da7e02370b8cd10aac39
Ludvig <l.lundh@hotmail.com>
2020-04-18 22:27:19 +0200

    add reducer

--- src/store/rootReducer.js
+++ src/store/rootReducer.js
@@ -1,6 +1,8 @@
 import {combineReducers} from '@reduxjs/toolkit';
 import themeReducer from '../services/theme/themeSlice';
+import rocketIdReducer from '../features/rockets/RocketIdSlice';
 
 export default combineReducers({
   theme: themeReducer,
+  rocketDetailId: rocketIdReducer,
 });
--- src/types/types.js
+++ src/types/types.js
@@ -1,1 +1,3 @@
 export const SET_TEXT = 'SET_TEXT';
+export const SET_ROCKET_DETAIL_ID = 'SET_ROCKET_DETAIL_ID';
+export const GET_ROCKET_DETAIL_ID= 'GET_ROCKET_DETAIL_ID';
