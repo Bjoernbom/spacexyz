a6f21ac50b020293c51a8c30847a0ea23cd15f98
Ludvig <l.lundh@hotmail.com>
2020-04-19 09:13:52 +0200

    update dashboard

--- src/features/dashboard/DashboardScreen.js
+++ src/features/dashboard/DashboardScreen.js
@@ -3,17 +3,39 @@ import styled from 'styled-components/native';
 import ListingCard from '../../components/ListingCard';
 import Screen from '../../components/Screen';
 import {SCREEN_NAMES} from '../../app/constants';
+import UpcomingLaunchComponent from '../../components/UpcomingLaunch';
 
-const Content = styled.View`
-  margin-top: 80px;
-  align-content: center;
-  flex: 1;
+const TopContent = styled.View`
+  margin-top: 60px;
+`;
+const Content = styled.ScrollView``;
+
+const Space = styled.View`
+  height: 20px;
+`;
+
+const Header = styled.Text`
+  color: ${props => props.theme.text.primary};
+  margin-bottom: 10px;
+  font-size: 22;
 `;
 
 export default function DashboardScreen({navigation}) {
   return (
     <Screen>
+      <TopContent>
+        <Header>Dashboard</Header>
+        <UpcomingLaunchComponent />
+      </TopContent>
+      <Space />
+      <Header>News</Header>
       <Content>
+        <ListingCard
+          onPress={() => navigation.navigate(SCREEN_NAMES.DETAILED_LISTING)}
+        />
+        <ListingCard
+          onPress={() => navigation.navigate(SCREEN_NAMES.DETAILED_LISTING)}
+        />
         <ListingCard
           onPress={() => navigation.navigate(SCREEN_NAMES.DETAILED_LISTING)}
         />
