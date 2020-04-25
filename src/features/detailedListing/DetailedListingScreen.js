1123834cac010129f190b58475b0c9af7e619729
Ludvig <l.lundh@hotmail.com>
2020-04-25 20:26:06 +0200

    update detaildListing

--- src/features/detailedListing/DetailedListingScreen.js
+++ src/features/detailedListing/DetailedListingScreen.js
@@ -3,7 +3,10 @@ import {Animated, Dimensions} from 'react-native';
 import BackButton from '../../navigation/BackButton';
 import {SharedElement} from 'react-navigation-shared-element';
 import {dashboardData} from '../../api/mock';
+import EvilIcons from 'react-native-vector-icons/EvilIcons';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 import styled from 'styled-components/native';
+import Moment from 'moment';
 const src = '../../assets/spacex.jpg';
 
 const screenHeight = Dimensions.get('window').height;
@@ -14,7 +17,8 @@ const Screen = styled.View`
   background-color: ${props => props.theme.backgrounds.primary};
 `;
 const Content = styled.View`
-  height: 100%;
+  align-self: center;
+  width: 100%;
   background-color: ${props => props.theme.backgrounds.primary};
 `;
 const ScrollContainer = styled.ScrollView`
@@ -22,7 +26,7 @@ const ScrollContainer = styled.ScrollView`
 `;
 
 const HeaderImage = styled.Image`
-  height: 400;
+  height: 250;
   width: 100%;
   align-self: center;
 `;
@@ -32,29 +36,113 @@ const TextWrapper = styled.View`
   align-items: flex-start;
 `;
 
-const Header = styled.Text`
+const MissionHeader = styled.Text`
+  color: ${props => props.theme.text.primary};
+  font-size: 20px;
+  align-self: center;
+  text-align: center;
+`;
+
+const RocketHeader = styled.Text`
   color: ${props => props.theme.text.primary};
   font-size: 24px;
-  align-self: flex-start;
+  margin-bottom: 5px;
+  align-self: center;
+  text-align: center;
 `;
 
-const Description = styled.Text`
-  align-self: flex-start;
-  margin-top: 6px;
+const Header = styled.Text`
   color: ${props => props.theme.text.primary};
-  font-size: 16px;
-  overflow: hidden;
+  font-size: 26px;
+  align-self: flex-start;
+  text-align: center;
 `;
 
-const Reg = styled.Text`
+const LineBreak = styled.View`
+  margin-top: 20px;
+  margin-bottom: 5px;
+  width: 100%;
+  height: 0.3px;
+  align-self: center;
+  background-color: ${props => props.theme.borders.secondary};
+`;
+
+const Text = styled.Text`
   margin-top: 6px;
-  font-size: 16px;
+  font-size: 20px;
   color: ${props => props.theme.text.primary};
+`;
+
+const EvilIcon = styled(EvilIcons).attrs(props => ({
+  color: props.theme.text.primary,
+}))`
+  font-size: 35px;
+  margin-right: 6px;
+  align-self: center;
+  justify-content: center;
+`;
+
+const MaterialCommunityIcon = styled(MaterialCommunityIcons).attrs(props => ({
+  color: props.theme.text.primary,
+}))`
+  font-size: 35px;
+  margin-right: 6px;
+  align-self: center;
+  justify-content: center;
+`;
+
+const ContentColumn = styled.View`
+  flex-direction: column;
+  margin-right: 20px;
+  margin-left: 20px;
+  justify-content: center;
+`;
+const QuickInfoRow = styled.View`
+  flex-direction: row;
+  margin-bottom: 10px;
+`;
+
+const Row = styled.View`
+  flex-direction: row;
+`;
+
+const QuickInfo = styled.View`
+  border-radius: 10;
+  margin-top: -50px;
+  width: 95%;
+  align-self: center;
+  background-color: ${props => props.theme.backgrounds.secondary};
+  shadow-color: ${props => props.theme.shadows.primary};
+  /* shadow-color: red; */
+  shadow-opacity: 0.7;
+  shadow-radius: 6;
+  elevation: 1;
+`;
+
+const Space = styled.View`
+  height: 20px;
 `;
 
 function DetailedListingScreen({navigation}) {
   const [scrollY] = useState(new Animated.Value(0));
-  const {missionName, flightNumber, hasFlewnBefore} = dashboardData;
+  const [opacity] = useState(new Animated.Value(0));
+  const {
+    missionName,
+    flightNumber,
+    hasFlewnBefore,
+    rocketName,
+    rocketBlock,
+    launchSiteName,
+    localLaunchDate,
+    landingSite,
+    orbit,
+  } = dashboardData;
+  const rocketString = `${rocketName} | Block ${rocketBlock}`;
+  const formatDate = Moment(localLaunchDate).format('YYYY:MM:DD:HH:mm');
+  const [year, month, days, hours, minutes] = formatDate.split(':').map(Number);
+
+  const launchDateString = `${hours}:${minutes} on ${days}/${month}, ${year}`;
+
   const reused = hasFlewnBefore ? 'Has flewn before' : 'First flight';
 
   const imageOpacity = scrollY.interpolate({
@@ -81,16 +169,40 @@ function DetailedListingScreen({navigation}) {
               <HeaderImage source={require(src)} />
             </SharedElement>
           </Animated.View>
-          <TextWrapper>
-            <Header>{missionName}</Header>
-            <Description>
-              Raketer är coool! Raketer är coool! Raketer är coool! Raketer är
-              coool! Raketer är coool!
-            </Description>
-            <Reg>Raketnamn</Reg>
-            <Reg>2020-13-37</Reg>
-            <Reg>Landningplats</Reg>
-          </TextWrapper>
+          <QuickInfo>
+            <TextWrapper>
+              <RocketHeader>{rocketString}</RocketHeader>
+              <MissionHeader>{missionName}</MissionHeader>
+              <LineBreak />
+            </TextWrapper>
+            <ContentColumn>
+              <QuickInfoRow>
+                <EvilIcon name="location" />
+                <Text>{launchSiteName}</Text>
+              </QuickInfoRow>
+              <QuickInfoRow>
+                <EvilIcon name="clock" />
+                <Text>{launchDateString}</Text>
+              </QuickInfoRow>
+              <QuickInfoRow>
+                <MaterialCommunityIcon name="airplane-landing" />
+                <Text>{landingSite}</Text>
+              </QuickInfoRow>
+            </ContentColumn>
+          </QuickInfo>
+          <Space />
+          <ContentColumn>
+            <Header>Mission Details</Header>
+            <Text numberOfLines={1}>{missionName}</Text>
+            <Row>
+              <Text>Type: </Text>
+              <Text>Testtest</Text>
+            </Row>
+            <Row>
+              <Text>Orbit: </Text>
+              <Text>{orbit}</Text>
+            </Row>
+          </ContentColumn>
         </Content>
       </ScrollContainer>
     </Screen>
